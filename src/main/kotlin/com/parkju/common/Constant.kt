415a1a411007d3d5f73b30b6f8e52a22c4adba17
Jeong-Uk Park <dnr1105@gmail.com>
2021-09-03 23:53:23 +0900

    Initial commit

---
+++ src/main/kotlin/com/parkju/common/Constant.kt
@@ -0,0 +1,16 @@
+package com.parkju.common
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.util.*
+
+object Constant {
+    val EMPTY_DATE = Date(0)
+    val EMPTY_SQL_DATE = java.sql.Date(0)
+    val EMPTY_SQL_TIME = java.sql.Time(0)
+    val EMPTY_SQL_TIMESTAMP = java.sql.Timestamp(0)
+    val EMPTY_LOCAL_DATE = LocalDate.of(1970, 1, 1)
+    val EMPTY_LOCAL_TIME = LocalTime.of(0, 0)
+    val EMPTY_LOCAL_DATETIME = LocalDateTime.of(1970, 1, 1, 0, 0)
+}
