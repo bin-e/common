b6d24e8a76d08c22f7c3331f84bcdd9cbf502fdb
Jeong-Uk Park <dnr1105@gmail.com>
2021-09-12 22:40:44 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,28 @@
 /**
-* 퍼블리싱
-*/
+ * 퍼블리싱
+ */
 import org.gradle.api.publish.PublishingExtension
 
 plugins {
     `maven-publish`
 }
 
-configure<PublishingExtension> { 
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+configure<PublishingExtension> {
+    publishing {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+                credentials {
+                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                }
             }
         }
-    }
-    publications {
-        register<MavenPublication>("gpr") {
-            from(components["java"])
+        publications {
+            register<MavenPublication>("gpr") {
+                from(components["java"])
+            }
         }
     }
 }
